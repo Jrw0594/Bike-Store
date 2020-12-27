3775ddbffc98b090b4d1ab1da8fee41a7a595c87
James Wilhelm <jrw0594@gmail.com>
2020-12-27 02:52:57 -0500

    final push

--- mail.php
+++ mail.php
@@ -8,6 +8,6 @@ $email = $_POST['email'];
 $content="From: $name";
 $recipient = "jrw0594@gmail.com";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
+mail($recipient, $subject, $content, $mailheader) or die("Heroku Doesn't allow for emails to be sent via php :(");
 echo "Email sent!";
 ?>
